e80ba247e19f43e46f3fa1de744a49c4c6738ab0
dev||Mcz <63181866+Mczgodpiggy@users.noreply.github.com>
2021-10-30 22:40:11 +0800

    Update index.js

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ disbut(client)
 const fetch = require("node-fetch")
 const commands = require('./help');
 const botapproverroleid = "your_role_id_here"
+require("dotenv").config()
 var defaultPrefix = 'your_prefix_here';
 
 client.on("ready", () => {
